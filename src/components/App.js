19caafdfaf3b62432158dd3811a2bd703bfe658e
Saurabhdombe2000 <saurabhdombe83@gmail.com>
2025-12-05 11:06:30 +0530

    commit msg

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const App = () => {
+  const relatives = ["Saurabh", "Vedant", "Akash", "Nikhil"];
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <ol key="relativeList">
+        {relatives.map((relative, index) => (
+          <li key={`relativeListItem${index + 1}`}>{relative}</li>
+        ))}
+      </ol>
     </div>
   )
 }
